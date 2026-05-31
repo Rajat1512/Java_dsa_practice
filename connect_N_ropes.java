8716c7d3047b8366beffaf70083df8f64486a511
Rajat Kanwar Chandel <kanwarrajat2@gmail.com>
2026-05-31 23:10:34 +0530

    Connecting N ropes in one rope with minimum cost

---
+++ connect_N_ropes.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class connect_N_ropes {
+    public static void main(String args[]){
+        int ropes[] = {4, 3, 2, 6};
+        int cost = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=0; i<ropes.length; i++){
+            pq.add(ropes[i]);
+        }
+
+        while(pq.size()>1){
+            int min = pq.remove();
+            int min2 = pq.remove();
+            cost += min+min2;
+            pq.add(min+min2);
+        }
+        
+        System.out.println(cost);
+    }
+}
