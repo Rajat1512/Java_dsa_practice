13dad648777cb1a7fbd579adce9793f3496aded2
Rajat Kanwar Chandel <kanwarrajat2@gmail.com>
2026-06-04 04:39:02 +0530

    String Compression

---
+++ compression.java
@@ -0,0 +1,23 @@
+public class compression {
+
+    public static String compress(String str){
+        String nstr = "";
+
+        for(int i=0; i<str.length(); i++){
+            Integer count = 1;
+            while(i<str.length()-1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            nstr += str.charAt(i);
+            if(count > 1){
+                nstr += count.toString();
+            }
+        }
+        return nstr;
+    }
+    public static void main(String args[]){
+        String str = "aaaabbbcccccdddeeeee";
+        System.out.println("Compressed string: " + compress(str));
+    }
+}
